ff5c5eb24e64787174714125b8d153390fd28721
Gordon Gyakyi <gordon.gyakyi@btinternet.com>
2020-03-27 17:03:14 +0000

    profile

--- frontend/src/Components/UserProfile.js
+++ frontend/src/Components/UserProfile.js
@@ -1,15 +1,32 @@
 import React from 'react'
+import axios from 'axios'
 
 class UserProfile extends React.Component {
 
   constructor() {
     super()
-    this.state = { 
+    this.state = {
     }
   }
 
+  handleSubmit(event) {
+    event.preventDefault()
+    // choices = []
+
+    axios.get('/api/categories', this.state.data, { headers: { Authorization: `Bearer ${auth.getToken()}` } })
+      .then(() => this.props.history.push('/'))
+      .catch(err => this.setState({ errors: err.response.data.errors }))
+  }
+
   render() {
-    return <h1>Hello User</h1>
+    return <section className="section">
+      <h1>Hello</h1>
+      <div className="tile is-ancestor">
+        <div className="tile">
+          {/* content */}
+        </div>
+      </div>
+    </section>
   }
 
 }
