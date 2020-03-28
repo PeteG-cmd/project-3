1e0c7bdf72fe390dfbba53a13b0e9627b877864b
Gordon Gyakyi <gordon.gyakyi@btinternet.com>
2020-03-28 18:50:10 +0000

    Minor changes to Profile

--- frontend/src/Components/UserProfile.js
+++ frontend/src/Components/UserProfile.js
@@ -18,12 +18,6 @@ class UserProfile extends React.Component {
     }
   }
 
-  // componentDidMount() {
-  //   axios.post('/api/profile', {}, { headers: { Authorization: `Bearer ${auth.getToken()}` } })
-  //     .then(() => this.props.history.push('/profile/:used_id'))
-  //     .catch(err => this.setState({ errors: err.response.data.errors }))
-  //   console.log(this.state.data)
-  // }
 
   componentDidMount() {
     if (auth.isLoggedIn()) {
@@ -35,52 +29,57 @@ class UserProfile extends React.Component {
     }
   }
 
-  // To capture changes in forms
-  // handleChange(event) {
-  //   choices.push(event.target.value)
-  //   this.setState({ data: { categories: choices } })
-  //   console.log(choices)
-  // }
-
-  handleSubmit(event) {
-    event.preventDefault()
-    // data here
-  }
-
   render() {
     console.log(this.state.user)
+    // const totalBooks = this.state.user.books.length
+    // console.log(totalBooks)
     return <main className="hero is-fullheight">
 
       <div className="hero-body">
         <div className="container"></div>
 
-        <section className="section">
+        <section className="section has-text-centered">
           <div className="tile is-ancestor">
             <div className="tile is-4 has-text-centered is-vertical is-parent">
               <div className="tile is-child box">
-                <p className="title">Profile Pic</p>
+                <p className="title">Profile Picture</p>
 
-                <figure className="image is-128x128">
+                <figure className="image is-128x128 has-image-centered">
                   <img className="is-rounded" src="https://bulma.io/images/placeholders/128x128.png" />
                 </figure>
 
               </div>
               <div className="tile is-child box">
-                <p className="title">Username: {this.state.user.username}</p>
-                <p className="title">Email: {this.state.user.email}</p>
-                <p className="title">Password: {this.state.user.password}</p>
+                <p className="title is-4">Username: {this.state.user.username}</p>
+                <p className="title is-4">Email: {this.state.user.email}</p>
+                <p className="title is-4">Password: {this.state.user.password}</p>
 
                 <button className="button is-light is-danger">
                   Edit
-            </button>
+                </button>
               </div>
             </div>
-            <div className="tile is-parent">
+            <div className="tile is-4 is-vertical is-parent">
               <div className="tile is-child box">
                 <p className="title">Bio</p>
-                <p>{this.state.user.userBio}</p>
+                <div className="box">
+                  <p>{this.state.user.userBio}</p>
+
+                </div>
+                <button className="button is-light is-danger">
+                  Edit
+                </button>
               </div>
 
+              <div className="tile is-child box">
+                <p className="title">Book Clubs</p>
+                <div className="tile is-child box">
+                  <p className="title is-5">Member of Book {} Clubs </p>
+                </div>
+                <button id="booksClubprofileButton" className="button is-light is-info">
+                  Book Clubs
+                </button>
+              </div>
             </div>
 
             <div className="tile is-parent">
@@ -89,31 +88,30 @@ class UserProfile extends React.Component {
 
 
                 <div className="tile is-child box">
-                  <p className="title is-5">Number of Books Read:</p>
+                  <p className="title is-5">Number of Books Read: </p>
                   <p>Latest Book Read:</p>
+                  {/* <p>Latest Book Read: {this.state.user.booksRead.length}</p> */}
                 </div>
 
                 <div className="tile is-child box">
                   <p className="title is-5">Number of Books Rated:</p>
                   <p>Latest Book Rated:</p>
-                  {/* <p>{this.state.user.userBio}</p>  */}
                 </div>
 
                 <div className="tile is-child box">
                   <p className="title is-5">Number of Books on Wishlist:</p>
                   <p>Latest Book Added:</p>
-                  {/* <p>{this.state.user.userBio}</p>  */}
-
+                  {/* <p>{this.state.user.booksWishlist}</p>  */}
                   <div>
 
                   </div>
 
-                  <div className="has-text-centered">
-                    <button className="button is-light is-danger">
-                      My Library
-                    </button>
-                  </div>
+                </div>
 
+                <div id="booksProfileButton" className="has-text-centered">
+                  <button className="button is-light is-info">
+                    My Library
+                  </button>
                 </div>
 
               </div>
@@ -125,7 +123,7 @@ class UserProfile extends React.Component {
           <div className="tile is-parent has-text-centered">
 
             <div className="box">
-              <h1 className="title">Favourite Catergories</h1>
+              <h1 className="title">Favourite Categories</h1>
               <form
                 className="form"
                 onSubmit={(event) => this.handleSubmit(event)}
@@ -136,168 +134,168 @@ class UserProfile extends React.Component {
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="advice-how-to-and-miscellaneous" />
                           Advice and How To
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="animals" />
                           Animals
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="business-books" />
                           Business
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="celebrities" />
                           Celebrities
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="paperback-books" />
                           Children’s Paperback Books
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="picture-books" />
                           Children’s Picture Books
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="mass-market-paperback" />
                           Fiction
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="paperback-nonfiction" />
                           Non-Fiction
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="crime-and-punishment" />
                           Crime and Punishment
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="culture" />
                           Culture
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="education" />
                           Education
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="espionage" />
                           Espionage
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="food-and-fitness" />
                           Food and Diet
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="fashion-manners-and-customs" />
                           Fashion
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="family" />
                           Parenthood and Family
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="games-and-activities" />
                           Games and Activities
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="graphic-books-and-manga" />
                           Graphic Books and Manga
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="health" />
                           Health
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="humor" />
                           Humour
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="relationships" />
                           Relationships
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="hardcover-political-books" />
                           Politics
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="race-and-civil-rights" />
                           Race and Civil Rights
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="religion-spirituality-and-faith" />
                           Religion, Spirituality and Faith
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="science" />
                           Science
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="sports" />
                           Sport and Fitness
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="travel" />
                           Travel
-            </label>
+                </label>
                 <label className="checkbox">
                   <input
                     onChange={(event) => this.handleChange(event)}
                     type="checkbox" value="young-adult" />
                           Young Adult
-            </label>
+                </label>
 
                 <div>
                   <button className="button is-light is-danger">
                     Edit
-              </button>
+                  </button>
                 </div>
               </form>
 
