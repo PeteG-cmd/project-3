0a4d14c809cafd3369a9859807084066e1634ccf
Gordon Gyakyi <gordon.gyakyi@btinternet.com>
2020-03-28 09:49:07 +0000

    Minor updates to Profile

--- frontend/src/Components/UserProfile.js
+++ frontend/src/Components/UserProfile.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import axios from 'axios'
 
+import auth from '../lib/auth'
+
 class UserProfile extends React.Component {
 
   constructor() {
@@ -18,12 +20,45 @@ class UserProfile extends React.Component {
       .catch(err => this.setState({ errors: err.response.data.errors }))
   }
 
+  // handleChange(event) {
+  //   choices.push(event.target.value)
+  //   this.setState({ data: { categories: choices } })
+  //   console.log(choices)
+  // }
+
   render() {
     return <section className="section">
-      <h1>Hello</h1>
+      <div className="dropdown"></div>
       <div className="tile is-ancestor">
-        <div className="tile">
-          {/* content */}
+        <div className="tile is-vertical is-8">
+          <div className="tile">
+            <div className="tile is-parent is-vertical">
+              <article className="tile is-child box">
+                {/* <!-- Put any content you want --> */}
+                <h1>Content Here</h1>
+                <p>Extend all along the top of the screen</p>
+              </article>
+              <article className="tile is-child box">
+                {/* <!-- Put any content you want --> */}
+                <p>Make this one a long </p>
+              </article>
+            </div>
+            <div className="tile is-parent">
+              <article className="tile is-child box">
+                {/* <!-- Put any content you want --> */}
+              </article>
+            </div>
+          </div>
+          <div className="tile is-parent">
+            <article className="tile is-child box">
+              {/* <!-- Put any content you want --> */}
+            </article>
+          </div>
+        </div>
+        <div className="tile is-parent">
+          <article className="tile is-child box">
+            {/* <!-- Put any content you want --> */}
+          </article>
         </div>
       </div>
     </section>
