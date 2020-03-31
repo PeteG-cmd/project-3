cb90b00a1bfbcd10d1964a9940088e497676f53a
Jamie Maxwell <jdsmax19@gmail.com>
2020-03-31 16:38:27 +0100

    commentsupdate

--- frontend/src/Components/BookComment.js
+++ frontend/src/Components/BookComment.js
@@ -2,6 +2,10 @@ import React from 'react'
 import axios from 'axios'
 import auth from '../lib/auth'
 import Spinner from './Common/Spinner'
+import Moment from 'react-moment'
+
+
+
 
 class BookComment extends React.Component {
 
@@ -26,7 +30,6 @@ class BookComment extends React.Component {
     axios.post(`/api/books/${bookId}/comments`, this.state.comment, { headers: { Authorization: `Bearer ${auth.getToken()}` } })
       .then(res => this.setState({ book: res.data }))
       .catch(err => this.setState({ error: err.response.data.message }))
-
   }
 
   handleChange(event) {
@@ -37,40 +40,58 @@ class BookComment extends React.Component {
   }
 
 
+  // setInterval(() => {
+  //   tubeStatus.innerHTML = `Tube Status: ${moment().format('LTS')}`
+  // }, 1000)
 
 
   render() {
     if (!this.state.book) return <Spinner />
+
+
     return <>
       <div className="allComments">
-        <article className="CommentContainer">
-          <figure className="CommentProfileImage">
-            <p className="image is-64x64">
-              <img src="https://bulma.io/images/placeholders/128x128.png"></img>
-            </p>
-          </figure>
-          {this.state.book.comments && this.state.book.comments.map((comment, index) => {
-            return <div key={index} className="AComment">
+        {this.state.book.comments && this.state.book.comments.map((comment, index) => {
+          const dateToFormat = `${comment.createdAt}`
+          return <article className="CommentContainer" key={index}>
+            <figure className="CommentProfileImage">
+              <p className="image is-64x64">
+                <img src="https://bulma.io/images/placeholders/128x128.png"></img>
+              </p>
+            </figure>
+
+            <div key={index} className="AComment">
               <div className="CommentsContent">
-
-
-                <p>{comment.comment}</p>
-
-
-
+                <p className="TheComment">{comment.comment}</p>
               </div>
-              <div className="LikeandReply">
-                <a className="Commenticons">
-                  <span className="icon is-small"><i className="fas fa-reply"></i></span>
-                </a>
-                <a className="Commenticons">
-                  <span className="icon is-small"><i className="fas fa-heart"></i></span>
-                </a>
+              <div className="CommentUserInfo">
+                <div className="IconsandName">
+                  <div className="Icons">
+                    <a className="IconSymbol">
+                      <span className="icon is-small"><i className="fas fa-heart"></i></span>
+                    </a>
+                    <a className="IconSymbol">
+                      <span id="replySymbol" className="icon is-small"><i className="fas fa-reply"></i></span>
+                    </a>
+                    <a className="IconSymbol">
+                      <span id="deleteSymbol" className="icon is-small"><i className="fas fa-times"></i></span>
+                    </a>
+                    <a className="IconSymbol">
+                      {/* MAKE THIS A MODAL */}
+                      <span id="editSymbol" className="icon is-small"><i className="fas fa-edit"></i></span> 
+                    </a>
+                  </div>
+                  <div className="CommentUserName">
+                    <p className="PostedBy"><strong>Posted By:</strong> {comment.user.username} </p>
+                  </div>
+                </div>
+                <div className="timeStamp">
+                  <p className="timeStampInfo"><Moment fromNow>{dateToFormat}</Moment></p>
+                </div>
               </div>
             </div>
-          })}
-        </article>
-
+          </article>
+        })}
       </div>
 
       <div className="addComment">
