c0c77556b8c7d282d78c52e08f676bd331196930
Jamie Maxwell <jdsmax19@gmail.com>
2020-04-02 12:31:43 +0100

    homepage

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -195,7 +195,7 @@ export default class Home extends React.Component {
                 <div className="LatestBookCard">
                   <div className="LatestBookCardImageContainer">
                     <figure>
-                      <img src={this.state.user.booksWishList[0].thumbnail} alt="" className="LatestBookCardContent"></img>
+                     <Link to='/mylibrary'><img src={this.state.user.booksWishList[0].thumbnail} alt="" className="LatestBookCardContent"></img></Link>
                     </figure>
                   </div>
                   <div className="SearchTitleandAuthorInfo">
